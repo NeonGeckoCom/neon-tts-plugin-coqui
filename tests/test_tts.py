c0350b9e848392a2b380ac9e64511350672b1d20
NeonBohdan <96498856+NeonBohdan@users.noreply.github.com>
2022-06-28 12:47:45 +0300

    Added Dutch lang (#36)

--- tests/test_tts.py
+++ tests/test_tts.py
@@ -43,7 +43,8 @@ class TestTTS(unittest.TestCase):
         ["fr", "Bonjour."],
         ["de", "Hallo."],
         ["pl", "Hej."],
-        ["uk", "Привіт."]
+        ["uk", "Привіт."],
+        ["nl", "Hallo."]
     ]
 
     @classmethod

ce3243ae2c705fab5110a6862a9bad9f3ca69d7c
NeonBohdan <bohdan@neon.ai>
2022-07-15 12:18:32 +0300

    Language config moved to a separate file

---
+++ neon_tts_plugin_coqui/configs.py
@@ -0,0 +1,29 @@
+languages = {
+    "en": {
+        "model": "neongeckocom/tts-vits-ljspeech-en@v0.3", 
+    },
+    "es": {
+        "model": "neongeckocom/tts-vits-css10-es@v0.2", 
+    },
+    "fr": {
+        "model": "neongeckocom/tts-vits-css10-fr@v0.2", 
+    },
+    "de": {
+        "model": "neongeckocom/tts-vits-css10-de@v0.2", 
+    },
+    "pl": {
+        "model": "neongeckocom/tts-vits-mai-pl@v0.5", 
+    },
+    "uk": {
+        "model": "neongeckocom/tts-vits-mai-uk@v0.8", 
+    },
+    "nl": {
+        "model": "neongeckocom/tts-vits-css10-nl@v0.2", 
+    },
+    "hu": {
+        "model": "neongeckocom/tts-vits-css10-hu@v0.1", 
+    },
+    "fi": {
+        "model": "neongeckocom/tts-vits-css10-fi@v0.1", 
+    },
+}
